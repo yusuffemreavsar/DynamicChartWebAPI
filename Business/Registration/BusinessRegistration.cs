4459dd03a546b4cbe0317ccd950f03226886fb16
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-12-07 14:47:19 +0300

    BusinessRegistration added.

---
+++ Business/Registration/BusinessRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Business.Abstract;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business.Registration
+{
+    public static class BusinessRegistration
+    {
+        public static void AddBusinessServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IDataBaseConnectionService, IDataBaseConnectionService>();
+        }
+    }
+}
--- DynamicChartWebAPI/Program.cs
+++ DynamicChartWebAPI/Program.cs
@@ -1,15 +1,15 @@
+using Business.Registration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services.AddBusinessServices();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
