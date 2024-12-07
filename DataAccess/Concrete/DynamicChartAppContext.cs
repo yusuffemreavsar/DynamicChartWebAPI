b90a6a7fc77bca00d1358a1369affc1f894d8e5a
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-12-07 14:02:54 +0300

    DynamicChartAppContext added. EntityFramework installed.

---
+++ DataAccess/Concrete/DynamicChartAppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class DynamicChartAppContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public DynamicChartAppContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+        }
+    }
+}
---
+++ Entities/DataBaseConnectionInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities
+{
+    public class DataBaseConnectionInfo
+    {
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
